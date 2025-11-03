3b50104e4067589a241803cd8faf862815c8d5b3
l9jmpaz <l9jmpaz@gmail.com>
2025-11-04 00:26:01 +0800

    222

--- src/components/Data.js
+++ src/components/Data.js
@@ -16,7 +16,7 @@ import {
 } from "lucide-react";
 import toast, { Toaster } from "react-hot-toast";
 
-const baseUrl = "https://smartcrop-backend-1.onrender.com/api";
+const baseUrl = "https://smartcrop-backend-in5e.onrender.com/api";
 
 export default function Data() {
   const [farmers, setFarmers] = useState([]);
