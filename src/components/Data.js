c2bbf08d3cc21ee2fef0ae4bbade22238511ea99
l9jmpaz <l9jmpaz@gmail.com>
2025-11-18 16:55:06 +0800

    Update Data.js

--- src/components/Data.js
+++ src/components/Data.js
@@ -165,7 +165,7 @@ setCropFilterList(cList);
   ============================================================ */
   const fetchYieldRecords = async () => {
     try {
-      const res = await axios.get(`${baseUrl}/farm/:userId/yield`);
+      const res = await axios.get(`${baseUrl}/farm/${userId}/yield`);
       if (res.data.success) setYieldRecords(res.data.data);
     } catch {
       toast.error("Failed to load yield data");
