8125da5cb12172f26d55e0456ee182d93d5eaf63
l9jmpaz <l9jmpaz@gmail.com>
2025-11-18 17:26:05 +0800

    Update Data.js22222

--- src/components/Data.js
+++ src/components/Data.js
@@ -612,12 +612,12 @@ setCropFilterList(cList);
 
                     <ResponsiveContainer width="100%" height="100%">
                       <LineChart data={yieldRecords.slice(0, 12).reverse()}>
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="date" />
-                        <YAxis />
-                        <Tooltip />
-                        <Line dataKey="yield" stroke="#10b981" />
-                      </LineChart>
+  <CartesianGrid strokeDasharray="3 3" />
+  <XAxis dataKey={(row) => new Date(row.date).toLocaleDateString()} />
+  <YAxis />
+  <Tooltip />
+  <Line dataKey="yield" stroke="#10b981" />
+</LineChart>
                     </ResponsiveContainer>
                   </div>
                 </div>
